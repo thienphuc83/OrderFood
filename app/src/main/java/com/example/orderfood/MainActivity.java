4a420709f65c0a0baf6fc0f036828e8dd1f04a2c
thienphuc <phuclt1976226@gmail.com>
2020-10-02 15:42:23 +0700

    splash screen

--- app/src/main/java/com/example/orderfood/MainActivity.java
+++ app/src/main/java/com/example/orderfood/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.orderfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import info.hoang8f.widget.FButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FButton btnSignIn, btnSignUp;
+    TextView tvLogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnhXa();
+        OnClick();
+    }
+
+    private void OnClick() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void AnhXa() {
+        btnSignIn = findViewById(R.id.btnsignin);
+        btnSignUp = findViewById(R.id.btnsignup);
+        tvLogan = findViewById(R.id.tvlogan);
+
+        //set font tvlogan
+        Typeface typeface= Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        tvLogan.setTypeface(typeface);
     }
 }
